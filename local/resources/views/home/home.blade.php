6414390d2a20aee491c36805b91daf2ce8bcb1e4
Unknown <vinhhuynh115@gmail.com>
2017-03-06 23:57:53 +0700

    Dev Home Layout
    Dev left area

--- local/resources/views/home/home.blade.php
+++ local/resources/views/home/home.blade.php
@@ -4,6 +4,43 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/home/home.css') }}">
 	<script type="text/javascript" src="{{ asset('/js/home/home.js') }}"></script>
 	<div id="home-page">
-		<p>Content of Home page</p>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3">
+					<div id="left-area">
+						<div class="widget-header">
+							<img src="http://dsvn.vn/images/widgetIcon.png">
+							<p>Thông tin hành trình</p>
+						</div>
+						<div class="form">
+							<form>
+							  <div class="form-group">
+							    <label for="GaDi">Ga đi</label>
+							    <input type="text" class="form-control" id="GaDi" placeholder="Ga đi">
+							  </div>
+							  <div class="form-group">
+							    <label for="GaDen">Ga đến</label>
+							    <input type="text" class="form-control" id="GaDen" placeholder="Ga đến">
+							  </div>
+							  <button type="submit" class="btn btn-default">Submit</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div id="center-area">
+						
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div id="right-area">
+						<div class="widget-header">
+							<img src="http://dsvn.vn/images/widgetIcon.png">
+							<p>Giỏ vé</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @stop
