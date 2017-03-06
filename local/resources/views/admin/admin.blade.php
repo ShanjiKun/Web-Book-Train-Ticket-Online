cc73a4f479c714af12624f45ec8caa3a48667182
Unknown <huyenptit95@gmail.com>
2017-03-06 15:57:13 +0700

    new css file

--- local/resources/views/admin/admin.blade.php
+++ local/resources/views/admin/admin.blade.php
@@ -1,8 +1,8 @@
 @extends('../master/master')
 @section('title','Admin')
 @section('content')
-	<link rel="stylesheet" type="text/css" href="{{ asset('/css/booking-information/booking-information.css') }}">
-	<script type="text/javascript" src="{{ asset('/js/booking-information/booking-information.js') }}"></script>
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/admin/admin.css') }}">
+	<script type="text/javascript" src="{{ asset('/js/admin/admin.js') }}"></script>
 	<div id="booking-information-page">
 		<p>Content of admin page</p>
 	</div>
