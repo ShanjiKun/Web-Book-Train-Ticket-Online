d7bc16984110a043bb01c3ea55ca6809713a4640
Unknown <vinhhuynh115@gmail.com>
2017-04-01 08:38:39 +0700

    Remove lib

--- local/resources/views/admin/admin-master.blade.php
+++ local/resources/views/admin/admin-master.blade.php
@@ -12,11 +12,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/lib/bootstrap-datepicker.css') }}" />
     <script type="text/javascript" src="{{ asset('/js/lib/bootstrap-datepicker.js') }}"></script>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
+    
 
 	<title>Admin Area :: @yield('title')</title>
 </head> 
