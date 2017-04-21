e595bc3762847339d81ce29bd48ac5d64859ea35
Unknown <vinhhuynh115@gmail.com>
2017-04-21 14:54:13 +0700

    Update trip data

--- local/database/seeds/StationStopTableSeeder.php
+++ local/database/seeds/StationStopTableSeeder.php
@@ -15,101 +15,101 @@ class StationStopTableSeeder extends Seeder
         DB::table('station_stop')->insert([
         	'trip_id' => 1,
         	'station_id' => 1,
-        	'date_arrive' => '2017-03-31 2:00:00',
-        	'date_leave' => '2017-04-01 8:00:00'
+        	'date_arrive' => '2017-04-20 2:00:00',
+        	'date_leave' => '2017-04-21 8:00:00'
         ]);
         DB::table('station_stop')->insert([
         	'trip_id' => 1,
         	'station_id' => 2,
-        	'date_arrive' => '2017-04-02 02:00:00',
-        	'date_leave' => '2017-04-02 02:15:00'
+        	'date_arrive' => '2017-04-22 02:00:00',
+        	'date_leave' => '2017-04-22 02:15:00'
         ]);
         DB::table('station_stop')->insert([
         	'trip_id' => 1,
         	'station_id' => 3,
-        	'date_arrive' => '2017-04-02 4:30:00',
-        	'date_leave' => '2017-04-02 5:00:00'
+        	'date_arrive' => '2017-04-22 4:30:00',
+        	'date_leave' => '2017-04-22 5:00:00'
         ]);
         DB::table('station_stop')->insert([
         	'trip_id' => 1,
         	'station_id' => 4,
-        	'date_arrive' => '2017-04-02 14:00:00',
-        	'date_leave' => '2017-04-02 14:15:00'
+        	'date_arrive' => '2017-04-22 14:00:00',
+        	'date_leave' => '2017-04-22 14:15:00'
         ]);
         DB::table('station_stop')->insert([
         	'trip_id' => 1,
         	'station_id' => 5,
-        	'date_arrive' => '2017-04-02 21:00:00',
-        	'date_leave' => '2017-04-02 6:00:00'
+        	'date_arrive' => '2017-04-22 21:00:00',
+        	'date_leave' => '2017-04-22 6:00:00'
         ]);
 
         DB::table('station_stop')->insert([
             'trip_id' => 2,
             'station_id' => 1,
-            'date_arrive' => '2017-03-31 2:00:00',
-            'date_leave' => '2017-04-01 11:00:00'
+            'date_arrive' => '2017-04-20 2:00:00',
+            'date_leave' => '2017-04-21 11:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 2,
             'station_id' => 2,
-            'date_arrive' => '2017-04-02 05:00:00',
-            'date_leave' => '2017-04-02 05:15:00'
+            'date_arrive' => '2017-04-22 05:00:00',
+            'date_leave' => '2017-04-22 05:15:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 2,
             'station_id' => 3,
-            'date_arrive' => '2017-04-02 7:30:00',
-            'date_leave' => '2017-04-03 8:00:00'
+            'date_arrive' => '2017-04-22 7:30:00',
+            'date_leave' => '2017-04-23 8:00:00'
         ]);
 
         DB::table('station_stop')->insert([
             'trip_id' => 3,
             'station_id' => 5,
-            'date_arrive' => '2017-03-31 2:00:00',
-            'date_leave' => '2017-04-01 09:00:00'
+            'date_arrive' => '2017-04-20 2:00:00',
+            'date_leave' => '2017-04-21 09:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 3,
             'station_id' => 4,
-            'date_arrive' => '2017-04-01 15:45:00',
-            'date_leave' => '2017-04-01 16:00:00'
+            'date_arrive' => '2017-04-21 15:45:00',
+            'date_leave' => '2017-04-21 16:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 3,
             'station_id' => 3,
-            'date_arrive' => '2017-04-02 01:30:00',
-            'date_leave' => '2017-04-02 02:00:00'
+            'date_arrive' => '2017-04-22 01:30:00',
+            'date_leave' => '2017-04-22 02:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 3,
             'station_id' => 2,
-            'date_arrive' => '2017-04-02 04:15:00',
-            'date_leave' => '2017-04-02 04:30:00'
+            'date_arrive' => '2017-04-22 04:15:00',
+            'date_leave' => '2017-04-22 04:30:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 3,
             'station_id' => 1,
-            'date_arrive' => '2017-04-02 22:30:00',
-            'date_leave' => '2017-04-03 6:00:00'
+            'date_arrive' => '2017-04-22 22:30:00',
+            'date_leave' => '2017-04-23 6:00:00'
         ]);
 
          DB::table('station_stop')->insert([
             'trip_id' => 4,
             'station_id' => 5,
-            'date_arrive' => '2017-03-31 9:00:00',
-            'date_leave' => '2017-04-01 11:00:00'
+            'date_arrive' => '2017-04-20 9:00:00',
+            'date_leave' => '2017-04-21 11:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 4,
             'station_id' => 4,
-            'date_arrive' => '2017-04-01 17:45:00',
-            'date_leave' => '2017-04-01 18:00:00'
+            'date_arrive' => '2017-04-21 17:45:00',
+            'date_leave' => '2017-04-21 18:00:00'
         ]);
         DB::table('station_stop')->insert([
             'trip_id' => 4,
             'station_id' => 3,
-            'date_arrive' => '2017-04-02 03:30:00',
-            'date_leave' => '2017-04-02 05:00:00'
+            'date_arrive' => '2017-04-22 03:30:00',
+            'date_leave' => '2017-04-22 05:00:00'
         ]);
     }
 }
