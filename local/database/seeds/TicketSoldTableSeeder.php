fd2712267a3d1f4c6e889c800e459a5cf2b6c54f
Unknown <vinhhuynh115@gmail.com>
2017-05-02 13:14:27 +0700

    Add ticket_cart, remove passenger

--- local/database/seeds/TicketSoldTableSeeder.php
+++ local/database/seeds/TicketSoldTableSeeder.php
@@ -16,115 +16,115 @@ class TicketSoldTableSeeder extends Seeder
         // S: sold
         // W: wait
         // A: available
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 1,
-        	'trip_id' => 1,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 1,
-        	'station_arrive_id' =>3,
-            'state' => 'S'
-        ]);
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 2,
-        	'trip_id' => 1,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 1,
-        	'station_arrive_id' =>3,
-            'state' => 'W'
-        ]);
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 81,
-        	'trip_id' => 1,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 1,
-        	'station_arrive_id' => 3,
-            'state' => 'S'
-        ]);
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 82,
-        	'trip_id' => 1,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 3,
-        	'station_arrive_id' => 5,
-            'state' => 'W'
-        ]);
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 123,
-            'trip_id' => 2,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 1,
-            'station_arrive_id' => 3,
-            'state' => 'S'
-        ]);
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 124,
-            'trip_id' => 2,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 3,
-            'station_arrive_id' => 5,
-            'state' => 'W'
-        ]);
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 203,
-            'trip_id' => 2,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 1,
-            'station_arrive_id' => 3,
-            'state' => 'S'
-        ]);
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 204,
-            'trip_id' => 2,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 3,
-            'station_arrive_id' => 5,
-            'state' => 'W'
-        ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 1,
+        // 	'trip_id' => 1,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 1,
+        // 	'station_arrive_id' =>3,
+        //     'state' => 'S'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 2,
+        // 	'trip_id' => 1,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 1,
+        // 	'station_arrive_id' =>3,
+        //     'state' => 'W'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 81,
+        // 	'trip_id' => 1,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 1,
+        // 	'station_arrive_id' => 3,
+        //     'state' => 'S'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 82,
+        // 	'trip_id' => 1,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 3,
+        // 	'station_arrive_id' => 5,
+        //     'state' => 'W'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 123,
+        //     'trip_id' => 2,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 1,
+        //     'station_arrive_id' => 3,
+        //     'state' => 'S'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 124,
+        //     'trip_id' => 2,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 3,
+        //     'station_arrive_id' => 5,
+        //     'state' => 'W'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 203,
+        //     'trip_id' => 2,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 1,
+        //     'station_arrive_id' => 3,
+        //     'state' => 'S'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 204,
+        //     'trip_id' => 2,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 3,
+        //     'station_arrive_id' => 5,
+        //     'state' => 'W'
+        // ]);
 
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 231,
-        	'trip_id' => 3,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 5,
-        	'station_arrive_id' =>3,
-            'state' => 'S'
-        ]);
-        DB::table('ticket_sold')->insert([
-        	'ticket_id' => 232,
-        	'trip_id' => 3,
-        	'date_sell' => '2017-03-30 00:00:00',
-        	'type_passenger_id' => 'NL',
-        	'station_leave_id' => 3,
-        	'station_arrive_id' => 1,
-            'state' => 'W'
-        ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 231,
+        // 	'trip_id' => 3,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 5,
+        // 	'station_arrive_id' =>3,
+        //     'state' => 'S'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        // 	'ticket_id' => 232,
+        // 	'trip_id' => 3,
+        // 	'date_sell' => '2017-03-30 00:00:00',
+        // 	'type_passenger_id' => 'NL',
+        // 	'station_leave_id' => 3,
+        // 	'station_arrive_id' => 1,
+        //     'state' => 'W'
+        // ]);
 
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 375,
-            'trip_id' => 4,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 5,
-            'station_arrive_id' =>3,
-            'state' => 'W'
-        ]);
-        DB::table('ticket_sold')->insert([
-            'ticket_id' => 376,
-            'trip_id' => 4,
-            'date_sell' => '2017-03-30 00:00:00',
-            'type_passenger_id' => 'NL',
-            'station_leave_id' => 5,
-            'station_arrive_id' =>3,
-            'state' => 'W'
-        ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 375,
+        //     'trip_id' => 4,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 5,
+        //     'station_arrive_id' =>3,
+        //     'state' => 'W'
+        // ]);
+        // DB::table('ticket_sold')->insert([
+        //     'ticket_id' => 376,
+        //     'trip_id' => 4,
+        //     'date_sell' => '2017-03-30 00:00:00',
+        //     'type_passenger_id' => 'NL',
+        //     'station_leave_id' => 5,
+        //     'station_arrive_id' =>3,
+        //     'state' => 'W'
+        // ]);
     }
 }
